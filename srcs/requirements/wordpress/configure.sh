de4d6c9eed489a9c0b06a1f971f8bb6e2bd26fcf
Okan Koca <oknakoca@gmail.com>
2024-09-15 14:54:10 +0200

    creating a random user

--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-# Function to handle termination signals
 terminate() {
-    echo "Terminating..."
+    echo "Terminating, bye.."
     pkill php-fpm
     exit 0
 }
 
-# Trap termination signals
 trap terminate SIGTERM SIGINT
 
-# Wait for the database to be ready
 until wp db check --allow-root; do
   echo "Waiting for database..."
   sleep 3
 done
 
-# Set up WordPress
 wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
 
 wp theme activate pixl
 
-# Optionally, set up additional settings
-# wp option update blogname "${WORDPRESS_SITE_TITLE}" --allow-root
-# wp option update blogdescription "Just another WordPress site" --allow-root
+wp user create random_user random_user@example.com --role=subscriber
 
-# Start PHP-FPM
 exec php-fpm
