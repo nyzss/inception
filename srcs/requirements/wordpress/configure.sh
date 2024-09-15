57b1b372d3743cc21837c9bec676dbbbf257fd0a
Okan Koca <oknakoca@gmail.com>
2024-09-15 15:07:00 +0200

    added allow root flag to wp-cli commands

--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -15,8 +15,8 @@ done
 
 wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
 
-wp theme activate pixl
+wp theme activate pixl --allow-root
 
-wp user create random_user random_user@example.com --role=subscriber
+wp user create random_user random_user@example.com --role=subscriber --allow-root
 
 exec php-fpm
