aa3d959dd64e8320a938ac22a0d74167541a69cd
Okan Koca <oknakoca@gmail.com>
2024-09-16 16:17:10 +0200

    some script tweaks but still not connecting

--- new/mariadb/start_script.sh
+++ new/mariadb/start_script.sh
@@ -2,28 +2,32 @@
 
 terminate() {
     echo "Terminating, bye.."
-    pkill mysqld
+    mysqladmin -u root -p"$MYSQL_ROOT_PASSWORD" shutdown
     exit 0
 }
 
-trap terminate SIGTERM SIGINT
+trap terminate TERM INT
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    # mysql_install_db --user=mysql --datadir=/var/lib/mysql
+    mysqld --initialize-insecure --user=mysql --datadir=/var/lib/mysql
 
     # Start MariaDB server
-    mysqld --user=mysql --datadir=/var/lib/mysql &
+    mysqld_safe --user=mysql --datadir=/var/lib/mysql &
 
     # Wait for the server to start up
     while ! mysqladmin ping -s --password='' 2>/dev/null; do
         sleep 1
     done
 
-    # Set root password and create necessary users/permissions
+    mysql -e "DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+    mysql -e "DROP DATABASE IF EXISTS test;"
+    mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+
     mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
     mysql -e "CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';"
     mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
-    # mysql -e "FLUSH PRIVILEGES;"
+    mysql -e "GRANT ALL ON *.* TO 'admin'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}' WITH GRANT OPTION;"
 
     if [ -n "${MYSQL_DATABASE}" ]; then
         mysql -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
@@ -43,4 +47,4 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 fi
 
 # Start MariaDB server
-exec mysqld --user=mysql --console
+exec mysqld_safe --user=mysql --console
