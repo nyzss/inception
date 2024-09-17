91ca1ab6cbf03e708d321dab73d22757abb59f36
Okan Koca <oknakoca@gmail.com>
2024-09-17 11:21:11 +0200

    init and running is done correctly

--- new/mariadb/docker-entrypoint.sh
+++ new/mariadb/docker-entrypoint.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
+
 set -eo pipefail
 
-# If command starts with an option, prepend mysqld
-if [ "${1:0:1}" = '-' ]; then
-    set -- mysqld "$@"
-fi
+wait_for_mysql() {
+    until mysql -u root -e "SELECT 1" &> /dev/null; do
+        echo "Waiting for MariaDB to be ready..."
+        sleep 2
+    done
+}
 
 # Skip initialization if it's already done
-if [ ! -d "/var/lib/mysql/mysql" ]; then
-    # Initialize MySQL data directory
-    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+if [ ! -d "/is_init" ]; then
+    mysqld --user=mysql --skip-networking &
+    pid="$!"
 
-    # Start MySQL server
-    /usr/sbin/mysqld --user=mysql --bootstrap << EOF
-USE mysql;
-FLUSH PRIVILEGES;
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
+    wait_for_mysql
 
-    # Run initialization scripts
-    /usr/sbin/mysqld --user=mysql --bootstrap < /docker-entrypoint-initdb.d/init.sql
+    # Wait for MySQL to be ready
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+    mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -e "FLUSH PRIVILEGES;"
+
+    echo "done" > /is_init
+
+    echo "Stopping MySQL server..."
+    kill "$pid"
+    wait "$pid"
 fi
 
 # Start MySQL server
 exec gosu mysql "$@"
+# exec mysqld --user=mysql "$@"
