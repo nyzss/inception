39371931cff707ba9c28d6dae18fbcf166e59296
Okan Koca <oknakoca@gmail.com>
2024-09-18 14:19:21 +0200

    changed order of commands inside configure.sh for wordpress

--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -13,6 +13,11 @@ wp core download --allow-root
 wp core config --dbhost="${WORDPRESS_DB_HOST}" --dbname="${WORDPRESS_DB_NAME}" \
   --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --allow-root
 
+until wp db check --allow-root; do
+  echo "Waiting for database..."
+  sleep 3
+done
+
 wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" \
   --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
   --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
@@ -21,8 +26,4 @@ wp theme activate pixl --allow-root
 
 wp user create "${USER_USERNAME}" "${USER_EMAIL}" --user_pass="${USER_PASSWORD}" --role=subscriber --allow-root
 
-until wp db check --allow-root; do
-  echo "Waiting for database..."
-  sleep 3
-done
 exec php-fpm8.2 -F -R
