ee3002562523beee77a695821f615bf279e0b665
Okan Koca <oknakoca@gmail.com>
2024-09-18 10:01:25 +0200

    did some major changes to db initialization and wordpress connection (added env variables fixed a lot of issues too)

--- srcs/requirements/mariadb/docker-entrypoint.sh
+++ srcs/requirements/mariadb/docker-entrypoint.sh
@@ -2,41 +2,33 @@
 
 set -eo pipefail
 
-wait_for_mysql() {
-    until mysql -u root -e "SELECT 1" &> /dev/null; do
-        echo "Waiting for MariaDB to be ready..."
-        sleep 2
-    done
-}
-
 # Skip initialization if it's already done
-if [ ! -d "/is_init" ]; then
+if [ ! -d "/var/lib/mysql/is_init" ]; then
+
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql
+
     mysqld --user=mysql --skip-networking &
     pid="$!"
 
-    wait_for_mysql
-
     # Wait for MySQL to be ready
+    until mysqladmin ping -h"localhost" --silent; do
+        echo "Waiting for MariaDB to be ready..."
+        sleep 2
+    done
+
     mysql -e "FLUSH PRIVILEGES;"
-    mysql -e "GRANT ALL ON *.* TO 'root'@'localhost' WITH GRANT OPTION;"
-    mysql -e "FLUSH PRIVILEGES;"
+    mysql -e "CREATE USER IF NOT EXISTS 'root'@'%';"
+    mysql -e "ALTER USER 'root'@'%' IDENTIFIED BY '';"
     mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
     mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
     mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
-    mysql -e "CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mysql -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;"
+    mysql -e "GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION;"
     mysql -e "FLUSH PRIVILEGES;"
 
-
-
-
-    echo "done" > /is_init
-
-    echo "Stopping MySQL server..."
-    kill "$pid"
+    echo "done" > /var/lib/mysql/is_init
+    mysqladmin -uroot -p${MYSQL_ROOT_PASSWORD} shutdown
     wait "$pid"
 fi
 
 # Start MySQL server
 exec gosu mysql "$@"
-# exec mysqld --user=mysql "$@"
--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -8,19 +8,23 @@ terminate() {
 
 trap terminate SIGTERM SIGINT
 
+# until mysqlcheck "${WORDPRESS_DB_NAME}" -u"${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASSWORD}" -h"${WORDPRESS_DB_HOST}"; do
 until wp db check --allow-root; do
   echo "Waiting for database..."
   sleep 3
 done
 
-if [ ! -d "/is_init" ]; then
+# echo -e "HERHERHEHREHREHRHERHEEEEEEEEE\n\n\n\n\n\n\nHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+
+# if [ ! -d "/is_init" ]; then
 
   wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
 
   wp theme activate pixl --allow-root
 
   wp user create "${USER_USERNAME}" "${USER_EMAIL}" --user_pass="${USER_PASSWORD}" --role=subscriber --allow-root
-  echo "done" > /is_init
-fi
+
+#   echo "done" > /is_init
+# fi
 
 exec php-fpm8.2 -F -R
