a3278bff898863bdc8fd3a9f20092be8047804dd
Okan Koca <oknakoca@gmail.com>
2024-09-18 15:29:24 +0200

    now wordpress inits just once

--- srcs/requirements/wordpress/configure.sh
+++ srcs/requirements/wordpress/configure.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
 
-# terminate() {
-#     echo "Terminating, bye.."
-#     pkill php-fpm
-#     exit 0
-# }
+set -eo pipefail
 
-# trap terminate SIGTERM SIGINT
+wait_db() {
+  until wp db check --allow-root; do
+    echo "Waiting for database..."
+    sleep 3
+  done
+}
 
-wp core download --allow-root
+if [ ! -d "/var/www/html/is_init" ]; then
+  wp core download --allow-root
 
-wp core config --dbhost="${WORDPRESS_DB_HOST}" --dbname="${WORDPRESS_DB_NAME}" \
-  --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --allow-root
+  wp core config --dbhost="${WORDPRESS_DB_HOST}" --dbname="${WORDPRESS_DB_NAME}" \
+    --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --allow-root
 
-until wp db check --allow-root; do
-  echo "Waiting for database..."
-  sleep 3
-done
+  wait_db
 
-wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" \
-  --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-  --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
+  wp core install --url="${WORDPRESS_SITE_URL}" --title="${WORDPRESS_SITE_TITLE}" \
+    --admin_user="${WORDPRESS_ADMIN_USER}" --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
+    --admin_email="${WORDPRESS_ADMIN_EMAIL}" --skip-email --allow-root
 
-wp theme install pixl --activate --allow-root
+  wp theme install pixl --activate --allow-root
 
-wp user create "${USER_USERNAME}" "${USER_EMAIL}" --user_pass="${USER_PASSWORD}" --role=subscriber --allow-root
+  wp user create "${USER_USERNAME}" "${USER_EMAIL}" --user_pass="${USER_PASSWORD}" --role=subscriber --allow-root
+
+  echo "done" > /var/www/html/is_init
+fi
+
+wait_db
 
 exec php-fpm8.2 -F -R
