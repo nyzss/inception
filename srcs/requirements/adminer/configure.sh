cc1e06a41133737a36374cd2c6fb696418403fed
Okan Koca <oknakoca@gmail.com>
2024-09-21 20:46:05 +0200

    setup adminer to only download once

--- srcs/requirements/adminer/configure.sh
+++ srcs/requirements/adminer/configure.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Downloading adminer..."
-curl -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -o index.php
+
+
+if [ ! -d "/var/www/html/adminer/index.php" ]; then
+	echo "Downloading adminer..."
+
+	curl -L https://github.com/vrana/adminer/releases/download/v4.8.1/adminer-4.8.1.php -o index.php
+fi
 
 echo "Started adminer!"
 exec php-fpm8.2 -F -R
