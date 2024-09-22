b52705fbc56ce81976e32c3b545d72d4da7c63c2
Okan Koca <oknakoca@gmail.com>
2024-09-22 11:29:23 +0200

    vsftpd now closes correctly, albeit slowly

--- srcs/requirements/ftp/start.sh
+++ srcs/requirements/ftp/start.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-handle_signal() {
+cleanup() {
     echo "Received signal, stopping vsftpd..."
-    kill -TERM "$child_pid"
-    wait "$child_pid"
+    pkill -P $$
     exit 0
 }
 
-# Set up signal traps
-trap handle_signal QUIT TERM INT EXIT
+for sig in INT QUIT HUP TERM; do
+  trap "
+    cleanup
+    trap - $sig EXIT
+    kill -s $sig "'"$$"' "$sig"
+done
+trap cleanup EXIT
 
-# Start vsftpd
 echo "Starting vsftpd..."
 /usr/sbin/vsftpd /etc/vsftpd.conf &
 echo "vsftpd started successfully!"
 
-# Store the PID of vsftpd
 child_pid=$!
 
-# Wait for vsftpd to exit
 wait "$child_pid"
