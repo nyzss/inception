f6fd10a260a9586bcdbaae0d0a0d206a7e3a6f3d
Okan Koca <oknakoca@gmail.com>
2024-09-22 11:59:36 +0200

    changes to vsftpd config, grafana is now in dev mode

--- srcs/requirements/ftp/start.sh
+++ srcs/requirements/ftp/start.sh
@@ -1,10 +1,38 @@
-#!/bin/bash
+# !/bin/bash
+
+# cleanup() {
+#     echo "Received signal, stopping vsftpd..."
+#     pkill -P $$
+#     exit 0
+# }
+
+# for sig in INT QUIT HUP TERM; do
+#   trap "
+#     cleanup
+#     trap - $sig EXIT
+#     kill -s $sig "'"$$"' "$sig"
+# done
+# trap cleanup EXIT
+
+# echo "Starting vsftpd..."
+# /usr/sbin/vsftpd /etc/vsftpd.conf &
+# echo "vsftpd started successfully!"
+
+# child_pid=$!
+
+# wait "$child_pid"
+
 
 cleanup() {
     echo "Received signal, stopping vsftpd..."
+    kill -TERM "$child_pid"
+    # wait "$child_pid"
     pkill -P $$
     exit 0
 }
+
+# Set up signal traps
+# trap cleanup QUIT TERM INT EXIT
 
 for sig in INT QUIT HUP TERM; do
   trap "
@@ -14,10 +42,13 @@ for sig in INT QUIT HUP TERM; do
 done
 trap cleanup EXIT
 
+# Start vsftpd
 echo "Starting vsftpd..."
 /usr/sbin/vsftpd /etc/vsftpd.conf &
 echo "vsftpd started successfully!"
 
+# Store the PID of vsftpd
 child_pid=$!
 
+# Wait for vsftpd to exit
 wait "$child_pid"
--- srcs/requirements/grafana/configure.sh
+++ srcs/requirements/grafana/configure.sh
@@ -2,13 +2,17 @@
 
 # grafana-cli  --homepath /usr/share/grafana plugins install grafana-github-datasource
 
-mkdir -p /var/lib/grafana/plugins
+grafana-cli plugins install grafana-github-datasource
+# grafana-cli --pluginsDir "/var/lib/grafana/plugins" --homepath /usr/share/grafana plugins install grafana-github-datasource
 
-wget https://github.com/grafana/github-datasource/releases/download/v1.8.2/grafana-github-datasource-1.8.2.linux_amd64.zip -O ./grafana-github-datasource.zip
+# mkdir -p /var/lib/grafana/plugins
 
-unzip ./grafana-github-datasource.zip && rm -rf ./grafana-github-datasource.zip
+# wget https://github.com/grafana/github-datasource/releases/download/v1.8.2/grafana-github-datasource-1.8.2.linux_amd64.zip -O ./grafana-github-datasource.zip
 
-mv ./grafana-github-datasource /var/lib/grafana/plugins/grafana-github-datasource
+# unzip ./grafana-github-datasource.zip && rm -rf ./grafana-github-datasource.zip
+
+# mv ./grafana-github-datasource /var/lib/grafana/plugins/grafana-github-datasource
+
+# chown -R grafana:grafana /var/lib/grafana/plugins
 
 exec grafana-server --homepath /usr/share/grafana --config /grafana.ini
-
